5631de6e333d01b702343b5efe85dba4c8ca1b73
Dansolo1235 <danielberihun92@gmail.com>
2023-07-23 16:08:30 -0500

    it prints hello world five times

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
     public static void main(String[] args) {
-    System.out.println("hello world");    
+    for(int i=1; i<=5;i++)System.out.println("hello world");   
     }
 }
