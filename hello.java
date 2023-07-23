684f86bbe9a0f4df8d97afedc2afdf3343dc11aa
Amanuel <amanuelsolomonbbdsa@gmail.com>
2023-07-23 16:31:08 -0500

    return to origin

--- hello.java
+++ hello.java
@@ -1,10 +1,5 @@
 public class hello{
-    
     public static void main(String[] args) {
-System.out.print("mango macarina");
-    }
-    static void counter(int j){
-for(int i=1; i<=j;i++)System.out.println("hello world");   
-    
+    System.out.println("hello world");    
     }
 }
